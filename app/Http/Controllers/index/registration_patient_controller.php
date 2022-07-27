ada1e380d07537dea4c9dc1f1c310394bda4ba41
Fts44 <calmajoseph7@gmail.com>
2022-07-27 12:47:47 +0800

    registration update

--- app/Http/Controllers/index/registration_patient_controller.php
+++ app/Http/Controllers/index/registration_patient_controller.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers\index;
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\otp_controller;
+
 use Illuminate\Http\Request;
-use App\Models\index\register_patient_model;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\index\register_patient_model;
+
+use App\Rules\gsuite_rule;
+
 
 class registration_patient_controller extends Controller
 {
@@ -14,17 +20,19 @@ class registration_patient_controller extends Controller
         $this->otp_controller = new otp_controller;
     }
     public function store(Request $request){
-        $request->validate(
-            [
-                'gsuite_email' => 'required|max:255|unique:accounts',
-                'otp' => 'required|numeric|digits:4',
-                'password' => 'required|min:6|max:60',
-                'cpassword' => 'required|same:password'
-            ],
-            [
-                'cpassword.same' => 'Password not match.'
-            ]
-        );
+  
+        $rules = [
+            'gsuite_email' => ['required','max:255','unique:accounts',new gsuite_rule],
+            'otp' => ['required','numeric','digits:4'],
+            'password' => ['required','min:6','max:60'],
+            'cpassword' => ['required','same:password']
+        ];
+
+        $messages = [
+            'cpassword.same' => 'Password not match.'
+        ];
+        
+        $validator = Validator::make( $request->all(), $rules, $messages);
 
         $verify_otp_request = new Request([
             'email' => $request->gsuite_email,
@@ -39,7 +47,7 @@ class registration_patient_controller extends Controller
             echo "registered";
         }
         else{
-            echo "invalid otp";
+            echo $validator->messages();
         }
     }
 }
